be731d8fa3ffd3e78115323b72f3ab22492374f6
krikaz <pascal.bou@protonmail.com>
2019-07-30 12:29:14 +0200

    a user now can only checks his recipes

--- auth/checkUserId-middleware.js
+++ auth/checkUserId-middleware.js
@@ -1,5 +1,6 @@
 module.exports = id => {
 	return function(req, res, next) {
+    console.log(id);
     console.log(req.decodedToken.subject);
 		if (req.decodedToken.subject && req.decodedToken.subject === id) {
 			next();
--- users/users-router.js
+++ users/users-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const Recipes = require('../recipes/recipes-model');
 const restricted = require('../auth/restricted-middleware');
-const checkUserId = require('../auth/checkUserId-middleware');
+// const checkUserId = require('../auth/checkUserId-middleware');
 
 router.get('/', restricted, (req, res) => {
 	Users.find()
@@ -13,13 +13,19 @@ router.get('/', restricted, (req, res) => {
 		.catch(err => res.status(500).send(err));
 });
 
-router.get('/:id/recipes', restricted, checkUserId(2), (req, res) => {
-	const { id } = req.params;
-	Recipes.findByUserId(id)
-		.then(recipes => {
-			res.status(200).json(recipes);
-		})
-		.catch(err => res.status(500).send(err));
+router.get('/:id/recipes', restricted, (req, res) => {
+	// console.log(typeof(req.decodedToken.subject));
+	// console.log(typeof(req.params.id));
+	if (req.decodedToken.subject && req.decodedToken.subject.toString() === req.params.id) {
+		const { id } = req.params;
+		Recipes.findByUserId(id)
+			.then(recipes => {
+				res.status(200).json(recipes);
+			})
+			.catch(err => res.status(500).send(err));
+	} else {
+		res.status(403).json({ message: 'you cannot check those' });
+	}
 });
 
 module.exports = router;
